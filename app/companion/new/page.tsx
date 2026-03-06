2fb4e660463099b14d3fe74e5e2a468642c24093
oussama2210 <oussamaqtx@gmail.com>
2026-03-06 02:08:21 +0100

    second commit

---
+++ app/companion/new/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NewCompanionPage() {
+    return (
+        <div>
+            <h1>New Companion</h1>
+        </div>
+    );
+}
---
+++ app/my-journy/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyJournyPage = () => {
+    return (
+        <div>MyJournyPage</div>
+    )
+}
+
+export default MyJournyPage
